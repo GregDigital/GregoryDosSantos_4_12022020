f08a9d4aa9c5dd9d2f1875653b5a01d1839e8818
GregDigital <68654973+GregDigital@users.noreply.github.com>
2021-03-11 18:57:01 +0100

    Validation message remerciement

--- modal.js
+++ modal.js
@@ -14,8 +14,6 @@ const formData = document.querySelectorAll(".formData");
 const bground = document.querySelector(".bground");
 const closeModalBtn = document.querySelector(".close");
 
-
-
 // LANCER MODAL ========================================================================
 
 // launch modal event
@@ -29,128 +27,150 @@ function launchModal() {
 //FERMETURE MODAL =======================================================================
 
 // close modal event
-closeModalBtn.addEventListener("click", function(event) {
-  event.preventDefault ();
+closeModalBtn.addEventListener("click", function (event) {
+  event.preventDefault();
   closeModal();
-  });
+});
 
-  // close modal form
-  function closeModal() {
-    bground.style.display = "none";
-    
-  }
+// close modal form
+function closeModal() {
+  bground.style.display = "none";
+}
 
+// CONST VALIDATION MANUEL FORMULAIRE =========================================================================
 
-// CONST VALIDATION FORMULAIRE =========================================================================
+const form = document.getElementById("form");
 
-const form = document.getElementById ('form');
-const errorFOrm = document.querySelector("formData");
-const firstName = document.getElementById ('first');
-const lastName = document.getElementById ('last');
-const eMail = document.getElementById ('email');
-const birthDate = document.getElementById ('birthdate');
-const quantityElt = document.getElementById ('quantity');
+const error = document.querySelector(".formData");
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const eMail = document.getElementById("email");
+const birthDate = document.getElementById("birthdate");
+const quantityElt = document.getElementById("quantity");
 const cityElt = document.querySelectorAll("input[type=radio]");
-const acceptCondition = document.getElementById ("checkbox1")
+const acceptCondition = document.getElementById("checkbox1");
+
+const modalBtnSubmit= document.querySelector(".btn-submit");
 
 // REGEX ======================================================================================
-const emailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+const emailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 
-form.addEventListener('submit', (e) => {
+
+// CONFIRMATION VALIDATION  =======================================================
+
+
+let firstElt;
+let lastElt;
+let mailElt;
+let BirthdayElt;
+let quantElt;
+let villeElt;
+let acceptElt;
+
+
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-})
+});
 
-function validate () {
+function validate() {
 
-  if (!firstName.value.match(/(.*[a-z]){2}/i) || firstName.value == ' ' || firstName.value == null || firstName.value.length < 2){
-    errorFOrm.innerHTML = "Veuillez remplir le champ Prénom";
+  if (
+    !firstName.value.match(/(.*[a-z]){2}/i) ||
+    firstName.value == " " ||
+    firstName.value == null ||
+    firstName.value.length < 2 
+  ) {
+    alert("Veuillez remplir le champ prenom ");
     return false;
-  };
- 
- if ((!lastName.value.match(/(.*[a-z]){2}/i) || lastName.value == ' ' || lastName.value == null || lastName.value.length < 2)) {
-  alert ("Veuillez remplir le champ Nom");
-  return false;
- };
+  }
+    else {firstElt = true};
 
-if (!emailFormat.test(eMail.value)) {
-  alert("Veuillez remplir une adresse mail valide");
-  return false;
-};
+  if (
+    !lastName.value.match(/(.*[a-z]){2}/i) ||
+    lastName.value == " " ||
+    lastName.value == null ||
+    lastName.value.length < 2
+  ) {
+    alert("Veuillez remplir le champ Nom");
+    return false;
+  }
+  else {lastElt = true};
+  
 
-if  (!birthDate.value.match(/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/)) {
-  alert("Veuillez remplir votre date de naissance");
-  return false;
-};
+  if (!emailFormat.test(eMail.value)) {
+    alert("Veuillez remplir une adresse mail valide");
+    return false;
+  }
+  else {mailElt = true};
 
-if (!quantityElt.value.match(/^[0-9]+$/)){
-  alert("Saisir un chiffre");
-  return false;
-};
+  if (
+    !birthDate.value.match(
+      /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/
+    )
+  ) {
+    alert("Veuillez remplir votre date de naissance");
+    return false;
+  }
+  else {BirthdayElt = true};
 
-let cityCheck = false;
-for (let city of cityElt) {  
-if (city.checked == true)
-{ cityCheck = true}
-}
-  if(!cityCheck) {
+  if (!quantityElt.value.match(/^[0-9]+$/)) {
+    alert("Saisir un chiffre");
+    return false;
+  }
+  else {quantElt = true};
+
+  let cityCheck = false;
+  for (let city of cityElt) {
+    if (city.checked == true) {
+      cityCheck = true;
+    }
+  }
+  if (!cityCheck) {
     alert("Merci de sélectionner une ville");
-  return false;
-  };
+    return false;
+  }
+  else {villeElt = true};
+
+  if (!acceptCondition.checked) {
+    alert("Merci de lire et accepter les conditions");
+    return false;
+  }
+  else {acceptElt = true};
 
 
-if (!acceptCondition.checked) {
-  alert("Merci de lire et accepter les conditions");
-  return false;
-};
-
-
-
-} 
-
-
-/*
-// THANKS CONST
-const modalBtnSubmit= document.querySelector(".btn-submit");
-const modalbgThanks = document.querySelector(".bgroundthanks");
-const closeBtnThanks = document.querySelector(".closeThanks");
-const btnThanks = document.querySelector(".btnthanks")
-*/
-
-// launch Thanks
-
-// close modal event
-
-/*
-// close modal event
-closeBtnThanks.addEventListener("click", function(event) {
-  event.preventDefault ();
-  closeModalThanks();
-  });
-
-  btnThanks.addEventListener("click", function(event) {
-    event.preventDefault ();
-    closeModalThanks();
-    });
-
-  // close modal form
-  function closeModalThanks() {
-    modalbgThanks.style.display = "none";
-    
+  if (firstElt == true && lastElt == true && mailElt  == true && BirthdayElt == true && quantElt == true && villeElt == true && acceptElt == true) {
+  modalbg.style.display = "none"
+    modalbgThanks.style.display = "block";
   }
 
 
+}
 
-    modalBtnSubmit.addEventListener("click", function(event) {
-      event.preventDefault ();
-      validate();
-      });
-    
-    function validate() {
-      modalbgThanks.style.display = "block";
-      modalbg.style.display = "none";
-    }
-  */
+// CONST Close Modal Thanks ===============================================================
 
 
+const closeThanks = document.querySelector(".closeThanks")
+const btnThanks = document.querySelector(".btnthanks")
+const modalbgThanks = document.querySelector(".bgroundthanks");
+
+// close modal thanks event ================================================================
+btnThanks.addEventListener("click", function (event) {
+  event.preventDefault();
+  closeModalThanks();
+});
+
+closeThanks.addEventListener("click", function (event) {
+  event.preventDefault();
+  closeModalThanks();
+});
+
+// FUNCTION close modal thanks =========================================================
+
+function closeModalThanks() {
+ 
+  modalbgThanks.style.display = "none";
+}
+
+//=======================================================================================
 
